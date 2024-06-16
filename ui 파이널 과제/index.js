b9ddaf614073c5384f2f528032d1a9489cddd711
misung11 <misung6915@snu.ac.kr>
2024-06-16 20:33:27 +0900

    fortune

---
+++ ui 파이널 과제/index.js
@@ -0,0 +1,41 @@
+$(window).on('load', function() {
+    setFlowBanner();
+})
+
+function setFlowBanner() {
+    const $wrap = $('.flow_banner');
+    const $list = $('.flow_banner .list');
+    let wrapWidth = $wrap.width();
+    let listWidth = $list.width();
+    const speed = 92; //1초에 몇픽셀 이동하는지 설정
+
+    //리스트 복제
+    let $clone = $list.clone();
+    $wrap.append($clone);
+    flowBannerAct()
+
+    //배너 실행 함수
+    function flowBannerAct() {
+        //무한 반복을 위해 리스트를 복제 후 배너에 추가
+        if (listWidth < wrapWidth) {
+            const listCount = Math.ceil(wrapWidth * 2 / listWidth);
+            for (let i = 2; i < listCount; i++) {
+                $clone = $clone.clone();
+                $wrap.append($clone);
+            }
+        }
+        $wrap.find('.list').css({
+            'animation': `${listWidth / speed}s linear infinite flowRolling`
+        });
+    }
+
+    // 마우스가 요소 위로 진입했을 때 일시정지
+    $wrap.on('mouseenter', function () {
+        $wrap.find('.list').css('animation-play-state', 'paused');
+    });
+
+    // 마우스가 요소에서 빠져나갈 때 재생
+    $wrap.on('mouseleave', function () {
+        $wrap.find('.list').css('animation-play-state', 'running');
+    });
+}
