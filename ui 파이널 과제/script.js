c667ef55e4930c5dafd65e3b361b5084c7e0ff6d
misung11 <162278050+misung11@users.noreply.github.com>
2024-06-16 20:35:05 +0900

    Add files via upload

---
+++ ui 파이널 과제/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const box5 = document.querySelector('.box5');
+
+    
+    setTimeout(() => {
+        window.location.href = 'result.html'
+    }, 500);
+});
